c4c97d566d75c8d41efdb2c8d8284007ce80564a
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-12-08 21:18:52 -0800

    Finish #2

---
+++ FrogFraternityBeta/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrogFraternityBeta.Models
+{
+    public class Post
+    {
+        [Key]
+
+        public int PostId { get; set; }
+
+        public int Username { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime? PostTime { get; set; }
+        }
+    }
